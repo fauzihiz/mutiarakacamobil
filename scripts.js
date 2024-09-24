8a85a7adc97443723ad673d8d1165867be8a1a36
fauzihiz <37166084+fauzihiz@users.noreply.github.com>
2024-09-24 21:06:00 +0700

    init build

---
+++ scripts.js
@@ -0,0 +1,14 @@
+function toggleFaq(element) {
+    // Find the answer div next to the clicked question
+    const answer = element.nextElementSibling;
+    
+    // Toggle the answer visibility
+    if (answer.style.display === "none" || answer.style.display === "") {
+      answer.style.display = "block";
+      element.classList.add('active'); // Add active class to change question color
+    } else {
+      answer.style.display = "none";
+      element.classList.remove('active'); // Remove active class to revert question color
+    }
+  }
+  
